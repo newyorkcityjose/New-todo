44e37f8d6f38f87cdc00ca1893315a778a97fc69
Jose Tavarez <newyorkcityjose@gmail.com>
2020-05-14 09:39:27 -0400

    Updates

---
+++ precache-manifest.0c4c7eb95454f214388c57588d789680.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79b9c73c84f3384c88b9c51c2b09705b",
+    "url": "/New-todo/index.html"
+  },
+  {
+    "revision": "ff4da563c9c74c6d2bb0",
+    "url": "/New-todo/static/css/main.64584ac1.chunk.css"
+  },
+  {
+    "revision": "ac5f3366fd7d81db224f",
+    "url": "/New-todo/static/js/2.b957b067.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/New-todo/static/js/2.b957b067.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff4da563c9c74c6d2bb0",
+    "url": "/New-todo/static/js/main.4a2707ae.chunk.js"
+  },
+  {
+    "revision": "4337dbfc2598e8032e71",
+    "url": "/New-todo/static/js/runtime-main.be1659fb.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/New-todo/static/media/logo.5d5d9eef.svg"
+  }
+]);
